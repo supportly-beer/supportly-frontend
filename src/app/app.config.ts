eeeefad65c2c06a0b951e8f86f166bddf063e030
Jeerrryyyy <j.braun@findologic.com>
2023-06-20 14:05:29 +0200

    Try fix url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = 'https://supportly.jevzo.com';
-export const GRPC_URL = 'http://grpc.jevzo.com'
+export const GRPC_URL = 'https://grpc.jevzo.com'
 //export const BACKEND_URL = 'http://localhost:8080';
