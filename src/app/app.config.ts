151c0386a87969b687b5fb5cd675db013c793531
Jeerrryyyy <j.braun@findologic.com>
2023-06-20 15:35:07 +0200

    fuck you

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,3 @@
 export const BACKEND_URL = 'https://supportly.jevzo.com';
-export const GRPC_URL = 'https://grpc.jevzo.com'
+export const GRPC_URL = 'https://jevzo.com:9091'
 //export const BACKEND_URL = 'http://localhost:8080';
