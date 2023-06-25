ca7df9bbeec94fc2208ed0a6ea47c26a2bca478c
Jeerrryyyy <j.braun@findologic.com>
2023-06-25 15:30:40 +0200

    Fix generate script

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+mkdir src/app/protos/generated
+cd src/app/protos
 protoc --plugin=protoc-gen-ts_proto=$(npm root)/.bin/protoc-gen-ts_proto --js_out="import_style=commonjs,binary:generated/" --ts_out="service=grpc-web:generated/" ticket-chat.proto
