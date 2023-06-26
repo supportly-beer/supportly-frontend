040db9afde6ac36ed88a30999227f77e376cbc14
Jeerrryyyy <j.braun@findologic.com>
2023-06-26 11:17:05 +0200

    Update backend url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,3 @@
 //export const BACKEND_URL = 'https://supportly.jevzo.com';
 export const GRPC_URL = 'http://localhost:9091'
-export const BACKEND_URL = 'http://localhost:8080';
+export const BACKEND_URL = 'http://localhost:8080/v1';
